2678faca3ba4f58bdc823a0cfde2b9f95bc40f4b
zetsuboy <coolcel5555@gmail.com>
2024-02-24 13:04:35 +1000

    config export change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    output: 'export',
     basePath: process.env.NODE_ENV === 'production' ? "/valantis-test-task" : "",
     images: {
         unoptimized: true,
